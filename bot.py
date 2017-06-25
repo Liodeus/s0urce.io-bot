fded636e6c9f14618257ad2db97c28912606ccd9
Liodeus <thibaultgalbourdin@live.fr>
2017-06-25 22:18:59 +0200

    change to the wordlist, add words, and different grab box for the image

--- bot.py
+++ bot.py
@@ -9,17 +9,18 @@ import words
 # Globals
 # --------------------
 
-x_pad = 469
-y_pad = 665
+x_pad = 531
+y_pad = 666
 
 
 def screenGrab():
-    box = (x_pad + 1, y_pad + 1, x_pad + 363, y_pad + 19)
+    box = (x_pad, y_pad,  x_pad + 234, y_pad + 19)
     im = ImageOps.grayscale(ImageGrab.grab(box))
     a = array(im.getcolors())
     a = a.sum()
-
+    print(a)
     return a
+
 
 
 def get_cords():
@@ -73,30 +74,21 @@ if __name__ == "__main__":
 
     while Flag:
         s = screenGrab()
-        print(s)
-        if s == 24004:
-            try:
-                writeWord("global")
-            except:
-                writeWord("thread")
 
-        if s == 26034:
-            try:
-                writeWord("joinnetworkclient")
-            except:
-                writeWord("getdatapassword")
+        while s == 20445:
+            writeWord("ghostfilesystem")
+            writeWord("createnewpackage")
+            s = screenGrab()
 
-        if s == 26043:
-            try:
-                writeWord("disconnectchannel")
-            except:
-                writeWord("getmysqldomain")
+        while s == 20524:
+            writeWord("systemgridtype")
+            writeWord("fileexpresslog")
+            s = screenGrab()
 
-        if s == 25396:
-            try:
-                writeWord("blockthreat")
-            except:
-                writeWord("httpbuffersize")
+        while s == 19040:
+            writeWord("datatype")
+            writeWord("download")
+            s = screenGrab()
 
         if s in words.words:
             test(s)
